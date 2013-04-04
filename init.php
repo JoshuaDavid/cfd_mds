171428066703ea360aa4e7ccccb3471fa80cca24
Debbie S Ma <dma@towhee.csun.edu>
2013-04-04 13:04:01 -0700

    Changes

--- init.php
+++ init.php
@@ -5,22 +5,22 @@
     // The number of options on the scale (for example, if the number is 7,
     // then there will be a scale from 1 to 7 where 1 is completely 
     // different and 7 is identical.
-    $num_scale_options = 11;
+    $num_scale_options = 7;
     // Where the datastore is located.
-    $datastore = "../cfd_mds/datastore.txt";
+    $datastore = "../../cfd_mds_data/datastore.txt";
     // Where the participants log is located.
-    $participants = "../cfd_mds/participants.txt";
+    $participants = "../../cfd_mds_data/participants.txt";
     // How many face pairs participants will compare in a single session.
     $job_size = 50;
     // The number of times each pair of faces will be compared. This is
     // 15 comparisons of face A to face B and also 15 comparisons of 
     // face B to face A.
-    $ip_limit = 1000;
+    $ip_limit = 10000;
     // How many comparisons (not how many jobs: this is important) can
     // come from a single IP address.
     $num_comparisons = 15;
     // The cooldown period between faces, in milliseconds.
-    $cooldown = 5000;
+    $cooldown = 50;
     // The confirmation code
     if(strlen($_REQUEST['confirmation']) > 1) {
         $confirmation = $_REQUEST['confirmation'];
