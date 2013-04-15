9aea2d870606ef33ee54f266ccae288efa59fa7a
Joshua David <Joshua@Joshua-David.net>
2013-04-15 10:47:58 -0700

    Added a way to get data as JSON
    Removed information like timestamp and IP so the data is safe.

---
+++ anonymizeddataasJSON.php
@@ -0,0 +1,34 @@
+<?
+include './init.php';
+$unsaferawdata = file_get_contents($datastore);
+$unsaferows = explode("\n", $unsaferawdata);
+$originalcolnames = array(
+    'face1_id',
+    'face2_id',
+    'face1_url',
+    'face2_url',
+    'similarity',
+    'confirmation_code',
+    'ip_address',
+    'timestamp'
+);
+$safecolumns = array(
+    'face1_id',
+    'face2_id',
+    'face1_url',
+    'face2_url',
+    'similarity'
+);
+$data = array();
+foreach($unsaferows as $unsaferow) {
+    $rawvalues = explode("\t", $unsaferow);
+    if(sizeof($rawvalues) > 1) {
+        foreach($originalcolnames as $index => $column) {
+            if(in_array($column, $safecolumns)) {
+                $safecomparison[$column] = trim($rawvalues[$index]);
+            }
+        }
+        $safedata[] = $safecomparison;
+    }
+}
+echo json_encode($safedata);
